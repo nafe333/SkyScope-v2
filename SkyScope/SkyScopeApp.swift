1d4ce22f319a98371349a2544e27c1938be116df
nafe333 <94410012+nafe333@users.noreply.github.com>
2025-10-13 00:15:32 +0300

    Initial Commit

---
+++ SkyScope/SkyScopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyScopeApp.swift
+//  SkyScope
+//
+//  Created by Nafea Elkassas on 13/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct SkyScopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
